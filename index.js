83c61547649fb1f7f1546d8f02a45196ab7eba0d
Sns Charmie <charlsoluwapelumi@gmail.com>
2022-05-14 21:15:26 +0100

    password validation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const eye = document.querySelector("#eye");
+const password = document.querySelector("#password");
+const type = password.getAttribute("type")
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -16,3 +19,12 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+eye.addEventListener("click", function () {
+    // toggle the type attribute
+    type === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("bi-eye-slash-fill");
+});
